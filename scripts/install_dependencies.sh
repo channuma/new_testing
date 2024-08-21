d1ae73b3e1eb1aa1d74d3e25b2f012371eb57007
channu <channuangadi15@gmail.com>
2024-08-21 12:53:50 +0530

    scripts has been added

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,8 @@
+sudo yum update
+sudo yum install ruby
+sudo yum install wget
+cd /home/ec2-user
+wget  https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo ./install auto
