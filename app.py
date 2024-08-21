be042ef016150ea4f6d1b5244ee10251079a2dcb
channu <channuangadi420@gmail.com>
2024-08-21 14:15:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 
 # Show a sidebar with additional information
-st.sidebar.title("About")
+st.sidebar.title("About hello")
 st.sidebar.write("This is a simple Streamlit app created as an example.")
