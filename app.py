56949afd21dff492326a1c13da51e16c8595e598
channu <channuangadi420@gmail.com>
2024-08-22 08:00:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 
 # Show a sidebar with additional information
-st.sidebar.title("About hello")
+st.sidebar.title("About hello channu")
 st.sidebar.write("This is a simple Streamlit app created as an example.")
